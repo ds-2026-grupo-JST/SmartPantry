c7f186b29c8bc2c2ae6618745d2037007b5e9102
tadecaminos <tadeocaminos15@gmail.com>
2026-09-10 20:09:58 -0300

    agrego app.component

---
+++ angular/src/app/app.component.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Frontend initial sanity check', () => {
+  it('should pass initial suite', () => {
+    expect(true).toBe(true);
+  });
+});
